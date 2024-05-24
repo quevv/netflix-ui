a7ae12bf420220445c6ea6120ea4b601b207e8da
Võ Văn Quế <quevovan01062001@gmail.com>
2024-05-24 11:55:43 +0700

    Update .env to secure source code

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { get } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0Isj7cvQzECDJnMgMrWfnEIparbjV16c",
-    authDomain: "netflix-clone-b43dc.firebaseapp.com",
-    projectId: "netflix-clone-b43dc",
-    storageBucket: "netflix-clone-b43dc.appspot.com",
-    messagingSenderId: "832598414033",
-    appId: "1:832598414033:web:79ad899443ff7696ce2bf0",
-    measurementId: "G-6DNYMHB2GK"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
